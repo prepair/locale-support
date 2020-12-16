cb32c70f45035b8c90b93294b7305fb5822da64e
Ferenc Mile <f.mile@mito.hu>
2020-12-16 09:03:37 +0100

    feat: remove deprecated moment code

--- lib-es5/index.js
+++ lib-es5/index.js
@@ -1,11 +1,9 @@
 'use strict';
 
-var momentLoader = require('./moment-loader');
 var metadata = require('./metadata');
 var modRewrite = require('./mod-rewrite');
 
 module.exports = {
-  getMomentLoader: momentLoader.getMomentLoader,
   getModRewriteRex: modRewrite.getPartialRex,
   metadata: metadata
 };
